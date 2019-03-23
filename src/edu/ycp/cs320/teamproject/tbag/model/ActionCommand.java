9df89862ae1201301ccf8774c1bf62e890820551
Jennifer Rhine <jrhine@ycp.edu>
2019-03-23 18:43:59 -0400

    Adding all of my classes and junit tests. I made sure they work!

---
+++ src/edu/ycp/cs320/teamproject/tbag/model/ActionCommand.java
@@ -0,0 +1,32 @@
+package edu.ycp.cs320.teamproject.tbag.model;
+
+public enum ActionCommand {
+	
+	//player's actions
+	TAKE,
+	
+	GRAB,
+	
+	READ,
+	
+	LOOK,
+	
+	LISTEN,
+	
+	SMELL,
+	
+	EAT,
+	
+	DRINK,
+	
+	LIGHT,
+	
+	WALK,
+	
+	RUN,
+	
+	JUMP,
+	
+	CRAWL;
+
+}
---
+++ src/edu/ycp/cs320/teamproject/tbag/model/MoveCommand.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs320.teamproject.tbag.model;
+
+public enum MoveCommand {
+
+	//commands for movement
+	NORTH,
+	
+	SOUTH,
+	
+	EAST,
+	
+	WEST,
+	
+	LEFT,
+	
+	RIGHT;
+	
+}
