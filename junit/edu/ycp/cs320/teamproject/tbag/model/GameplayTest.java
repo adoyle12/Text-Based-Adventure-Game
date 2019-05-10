c99f56ed138a16583ccbc33f1ba046c26687ac41
Jennifer Rhine <jrhine@ycp.edu>
2019-05-10 15:30:38 -0400

    Gameplay Test
    Passes all green in coverage too

--- junit/edu/ycp/cs320/teamproject/tbag/model/GameplayTest.java
+++ junit/edu/ycp/cs320/teamproject/tbag/model/GameplayTest.java
@@ -3,6 +3,7 @@ package edu.ycp.cs320.teamproject.tbag.model;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +16,7 @@ public class GameplayTest
 	private ArrayList<String> output3;
 	private String input = "0";
 	private String input2 = "1";
+	private List<String> inventory;
 	
 	@Before
 	public void setUp() 
@@ -24,6 +26,8 @@ public class GameplayTest
 		output = new ArrayList<String>();
 		output2 = new ArrayList<String>();
 		output3 = new ArrayList<String>();
+		
+		model.addInventory(inventory);
 	
 	}
 	
@@ -37,20 +41,29 @@ public class GameplayTest
 	@Test
 	public void testGetSetAddOutput() {
 		
+		//test getting and setting output
 		model.setOutput(output);
 		assertEquals(output, model.getOutput());
 		
+		//test changing output to output2
 		output2.add(input);
 		
 		model.setOutput(output2);
 		assertEquals(output2, model.getOutput());
+		assertTrue(output2.contains(input));
 		
+		//test adding output3 to output2
 		output3.add(input2);
 		
 		model.addOutput(output3);
-		assertTrue(model.getOutput().contains(input));
-		assertTrue(model.getOutput().contains(input2));
-		
+		assertEquals(output3, model.getOutput());
+		assertTrue(output3.contains(input2));
+	}
+	
+	@Test
+	public void testGetAddInventory() {
+		//test that inventory was added correctly
+		assertEquals(inventory, model.getInventory());
 	}
 	
 }
